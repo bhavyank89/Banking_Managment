d887f08c72a69b9529b4788df6e9485a75bc13f9
bhavyank89 <bhavyanksarolia@gmail.com>
2025-07-30 09:55:53 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include "Bank.cpp"
+
 using namespace std;
 
-void welcome(); 
+void welcome()
+{
+    cout << "==============================\n";
+    cout << "     BANKING MANAGEMENT SYSTEM\n";
+    cout << "==============================\n";
+}
 
-int main() {
+int main()
+{
+    Bank bank;
     welcome();
+
+    int choice;
+    do
+    {
+        cout << "\n1. Create Customer\n";
+        cout << "2. Create Account\n";
+        cout << "3. Login to Account\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            bank.createCustomer();
+            break;
+        case 2:
+            bank.createAccount();
+            break;
+        case 3:
+            bank.login();
+            break;
+        case 4:
+            cout << "Thank you for using the system.\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 4);
+
     return 0;
 }
